4687fcf8b868d39ca7648f6e07acf80e397ae574
pavel-slepenkov <pavel.slepenkov@gmail.com>
2020-11-24 08:19:31 +0000

    Deploying to gh-pages from  @ fd0ff740d42159fe49a0153b54c531f831702232 🚀

---
+++ _next/static/swXu2N62rg8VEPJxnpbOC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-a9578a083330cf4eeba2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-dfececddb39a358ab407.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
