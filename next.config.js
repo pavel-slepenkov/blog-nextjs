f4d649822ac66c83fef0976f1508e0ffa1e1d056
Pavel Slepenkov <pavel.slepenkov@gmail.com>
2021-01-04 09:02:59 +0300

    fix broken styles & posts links

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const isProduction = (process.env.NODE_ENV || 'production') === 'production'
 module.exports = {
     basePath: isProduction ? '/blog-nextjs' : '',
     assetPrefix: isProduction ? '/blog-nextjs/' : '',
