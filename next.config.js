634df0bc16a4c5964820ec2eb341ab0e974aabb4
Pavel Slepenkov <pavel.slepenkov@gmail.com>
2021-01-05 04:30:23 +0300

    add custom domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProduction = (process.env.NODE_ENV || 'production') === 'production'
 module.exports = {
-    basePath: isProduction ? '/blog-nextjs' : '',
-    assetPrefix: isProduction ? '/blog-nextjs/' : '',
+    // basePath: isProduction ? '/blog-nextjs' : '',
+    // assetPrefix: isProduction ? '/blog-nextjs/' : '',
     env: {
         BACKEND_URL: '/blog-nextjs',
     },
