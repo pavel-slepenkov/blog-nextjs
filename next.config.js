d26613d85963d8547c0ca385fec073795a640008
Pavel Slepiankou <pavel.slepenkov@gmail.com>
2021-01-21 09:11:28 +0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const isProduction = (process.env.NODE_ENV || 'production') === 'production'
-module.exports = {
-    // basePath: isProduction ? '/blog-nextjs' : '',
-    // assetPrefix: isProduction ? '/blog-nextjs/' : '',
-    env: {
-        BACKEND_URL: '/blog-nextjs',
-    },
-}
