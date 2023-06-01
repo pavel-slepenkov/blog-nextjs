3773a1207790d189ef394cd5ac75f32616e1b871
pavel-slepenkov <pavel.slepenkov@gmail.com>
2023-06-01 06:38:49 +0000

    Deploying to gh-pages from  @ a78ca1f85710f7ae705ff14fc14b51b564b819d7 🚀

---
+++ _next/static/9HxoGffdUL-kOR0x0J2xH/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fbook\u002F[id]","\u002Fbooks","\u002Fcv","\u002Fposts\u002F[id]","\u002Ftag\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/LnFaqV88K4tfUq672tjF2/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fcv","\u002Fabout","\u002F","\u002Fposts\u002F[id]","\u002Ftag\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
